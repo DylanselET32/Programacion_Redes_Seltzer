b0d77a03af6a6d0e7990888c9060b7b856d6b063
Dylan <91755493+DylanselET32@users.noreply.github.com>
2023-03-22 12:41:30 -0300

    Primera clase con comentarios y funciones de prints basicos y errores

---
+++ flujoDeDatos/src/main/java/flujoDeDatos/main.java
@@ -0,0 +1,46 @@
+package flujoDeDatos;
+
+import java.io.PrintStream;
+import java.util.logging.Logger;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String nombre = "Dylan";
+		int edad = 19;
+		
+		System.out.printf("Mi nombre es %S y tengo %D", nombre,edad);
+		System.out.append("Este codigo se ingresa seguido al anterior")
+		System.out.flush(); // limpia el flujo de datos
+		
+		System.out.write(64); //el write agrega al flujo de datos un caracter con el 
+		System.out.write(10); //10 en el codigo ascii, significa que envia el flujo de datos a la consola
+		
+		//enviar varios codigos con un array, dice "hola mundo"
+		byte[] array = {72,111,108,97, 32 ,109,117,110,100,111}; //array de bytes del codigos ASCII
+
+		//manejos de errores
+		try {
+			System.out.write(array); //imprimo 
+			System.out.println();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+		
+		Logger.getLogger(main.class.getName()).log(Level.SEVERE, "Error critico en el sistema.");  //son errores personalizados complejos
+		
+		PrintStream err = new PrintStream(System.err);
+		PrintStream ps = new PrintStream(System.out);
+		ps.println("Holaa");
+		
+		
+		
+		
+	}
+
+}
