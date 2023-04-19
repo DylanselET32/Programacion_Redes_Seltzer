3e9d51c5446e813b521db87f72166cc706c37caa
Dylan <91755493+DylanselET32@users.noreply.github.com>
2023-04-19 11:49:46 -0300

    Termino de colleciones

--- flujo_de_datos/src/main/java/flujo_de_datos/Colecciones.java
+++ flujo_de_datos/src/main/java/flujo_de_datos/Colecciones.java
@@ -3,10 +3,14 @@ package flujo_de_datos;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 
 public class Colecciones {
@@ -43,7 +47,37 @@ public class Colecciones {
 		inglesEspañol.put("bye", "chau");
 		
 		ps.println(inglesEspañol.get("Hello"));    
-		ps.println(inglesEspañol.get("bye"));    
+		ps.println(inglesEspañol.get("bye"));   
+		
+		//metodos de Hash Map
+		
+		inglesEspañol.containsKey("Hello");
+		inglesEspañol.replace("hi", "chau");
+		
+		//TREEMAP - Este map esta ordenado de menor a mayor 
+		TreeMap<Integer, String> DNI = new TreeMap<Integer, String>();		
+		DNI.put(10, "Dylan"); 
+		DNI.put(100, "Diego");
+		DNI.put(50,	 "pepe");
+		
+		
+		//Linked Hash Map
+		LinkedHashMap<Integer, Integer> alumnos = new LinkedHashMap<Integer, Integer>();
+		
+		//HashSET
+		HashSet<Integer> conjuntoNumeros= new HashSet<Integer>();
+		Set<Integer> conjuntos2= new HashSet<Integer>();
+		conjuntos2.contains(3);
+		
+		conjuntoNumeros.add(5);
+		conjuntoNumeros.add(6);
+		conjuntoNumeros.add(7);
+		conjuntoNumeros.add(5);
+		
+		
+		
+		
+		
 		//Maneras de iterar o recorrer
 		
 		//for tradicional
@@ -74,10 +108,18 @@ public class Colecciones {
 			String value = inglesEspañol.get(key);
 		}
 		
+		//Iterar por Entry
 		
+		for (Map.Entry<String,String> item: inglesEspañol.entrySet()) {
+			ps.print(item.getKey() + "\t");
+			ps.println(item.getValue());
+		}
 		
+		//CONVERSION DE COLLECTIONS
 		
-		
+		//MAP -> KeySet
+		Set<String> key = inglesEspañol.keySet();
+		ArrayList<String> values = (ArrayList<String>)inglesEspañol.values();
 		
 		
 	}
